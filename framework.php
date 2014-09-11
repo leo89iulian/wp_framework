9ddbbd5ef21dc86c8f06019fe3d648ed8eb7fc87
leo89iulian <leo@verycreative.info>
2014-09-11 17:53:22 +0300

    singleton pattern

--- framework.php
+++ framework.php
@@ -1,5 +1,16 @@
 <?php 
 class vc{
+	public static $_instance;
+        
+	private function __construct($args = null) {
+		// coodul aici
+	}
+	public static function getInstance($args = null){
+		if(!(self::$_instance instanceof self)) {
+			self::$instance = new vc($args);
+		}
+		return self::$_instance;
+	}
 	public function cache($k,$time="+1 hour",$v=NULL){
 		if($v===NULL){
 			$time_c=get_option("cache_".$k."_time",1);
